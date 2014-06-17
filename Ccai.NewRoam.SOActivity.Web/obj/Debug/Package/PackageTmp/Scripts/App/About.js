e6e6d18d65bcddf013b11120f1edccc958a555e7
webster7771 <oetawan@gmail.com>
2014-06-17 02:29:43 -0700

    Role module

---
+++ Ccai.NewRoam.SOActivity.Web/obj/Debug/Package/PackageTmp/Scripts/App/About.js
@@ -0,0 +1,8 @@
+﻿/// <reference path="../jquery-1.8.2.js" />
+
+(function () {
+    $(function () {
+        $('div[data-role="navbar"] li#AboutMenu a').addClass('ui-btn-active');
+        //$('div[data-role="navbar"] li#AboutMenu a').addClass('ui-state-persist');
+    });
+}());
